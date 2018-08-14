5ef179ae2bac1c0a0c828a3fbf7e29e5a7093fce
MIT ISMitistT <mitist@Justins-MacBook-Pro-3.local>
2018-08-14 13:49:33 -0400

    Pulled reader setup out into its own function

--- distill.py
+++ distill.py
@@ -6,14 +6,19 @@ import re
 import sys
 
 ip_regex = re.compile('\d{1,3}\.' + sys.argv[1] + '\.\d{1,3}\.\d{1,3}')
-file_names = sorted((glob("*-active_IPs.csv")))
+file_names = sorted((glob("../*-active_IPs.csv")))
 n_sets = []
 y_set = set()
 current_set = set()
 
+def get_reader(file_name):
+	return csv.reader(x.replace('\0', '') for x in open(file_name))
+
 for file_name in file_names:
 	# Remove null bytes for corruption in some files e.g. 2018-08-08
-	file_data = csv.reader(x.replace('\0', '') for x in open(file_name))
+	file_data = get_reader(file_name)
+	#print (file_data)
+	#csv.reader(x.replace('\0', '') for x in open(file_name))
 	n_set = set()
 
 	for IP in [row[1] for row in file_data if ip_regex.match(row[1]) and row[14] == 'N']:
@@ -22,7 +27,7 @@ for file_name in file_names:
 
 not_checked_in_IPs = set.intersection(*n_sets)
 
-file_data = csv.reader(x.replace('\0', '') for x in open(file_names[-1]))
+file_data = get_reader(file_names[-1])
 for IP in [row[1] for row in file_data if ip_regex.match(row[1]) and row[14] == 'N']:
 	current_set.add(IP)
 
@@ -32,7 +37,7 @@ print (len(ghost_ips))
 for ip in ghost_ips:
 	missing = []
 	for file_name in file_names:
-		file_data = csv.reader(x.replace('\0', '') for x in open(file_name))
+		file_data = get_reader(file_name)
 		if ip not in [row[1] for row in file_data if ip_regex.match(row[1]) and row[14] == 'N']:
 			missing.append(file_name)
 	print (ip + " is missing from '" + missing[0] + "' and " + str(len(missing) - 1 )  + " others")
