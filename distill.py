4ee3a1f36737d52a842a2f91f6d371bbb94dfa6a
Justin Francos <jfrancos@mit.edu>
2018-08-15 16:26:39 -0400

    created hostname dict to show hostnames

--- distill.py
+++ distill.py
@@ -3,14 +3,14 @@
 import csv
 from glob import glob
 import re
-import time
 import sys
+import time
 
 ip_regex = re.compile('\d{1,3}\.' + sys.argv[1] + '\.\d{1,3}\.\d{1,3}')
 file_names = sorted((glob("../*-active_IPs.csv")))
 y_set = set()
-#current_set = set()
 n_dictionary = {}
+hostname_dict = {}
 
 def get_reader(file_name):
 	# Remove null bytes for corruption in some files e.g. 2018-08-08
@@ -21,13 +21,14 @@ for file_name in file_names:
 	n_dictionary[file_name] = set(row[1] for row in file_data if ip_regex.match(row[1]) and row[14] == 'N')
 
 not_checked_in_IPs = set.intersection(*n_dictionary.values())
-
-#for IP in n_dictionary[file_names[-1]]:
-	#current_set.add(IP)
 current_set = n_dictionary[file_names[-1]]
-
 ghost_ips = current_set - not_checked_in_IPs
+
 print (len(ghost_ips))
+
+file_data = get_reader(file_names[-1])
+for row in file_data:
+	hostname_dict[row[1]] = row[2]
 
 for ip in ghost_ips:
 	missing = []
@@ -35,6 +36,6 @@ for ip in ghost_ips:
 		file_data = get_reader(file_name)
 		if ip not in n_dictionary[file_name]:
 			missing.append(file_name)
-	print (ip + " is missing from '" + missing[0] + "' and " + str(len(missing) - 1 )  + " others")
+	print ("{} ({}) is missing from '{}' and {} others".format(ip, hostname_dict[ip], missing[0], len(missing) - 1 ))
 
 print (len(not_checked_in_IPs))
