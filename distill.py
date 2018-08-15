4fec42ea0239643407803859eab9d342c60f66eb
Justin Francos <jfrancos@mit.edu>
2018-08-15 14:46:47 -0400

    makes a dictionary of relevant IPs now instead of searching the csv over and over, and now finishes in 1.5s instead of 20s

--- distill.py
+++ distill.py
@@ -3,6 +3,7 @@
 import csv
 from glob import glob
 import re
+import time
 import sys
 
 ip_regex = re.compile('\d{1,3}\.' + sys.argv[1] + '\.\d{1,3}\.\d{1,3}')
@@ -10,23 +11,25 @@ file_names = sorted((glob("../*-active_IPs.csv")))
 n_sets = []
 y_set = set()
 current_set = set()
+n_dictionary = {}
 
 def get_reader(file_name):
+	# Remove null bytes for corruption in some files e.g. 2018-08-08
 	return csv.reader(x.replace('\0', '') for x in open(file_name))
 
 for file_name in file_names:
-	# Remove null bytes for corruption in some files e.g. 2018-08-08
 	file_data = get_reader(file_name)
-	n_set = set()
+	n_dictionary[file_name] = [row[1] for row in file_data if ip_regex.match(row[1]) and row[14] == 'N']
 
-	for IP in [row[1] for row in file_data if ip_regex.match(row[1]) and row[14] == 'N']:
+for file_name in file_names:
+	n_set = set()
+	for IP in n_dictionary[file_name]:
 		n_set.add(IP)
 	n_sets.append(n_set)
 
 not_checked_in_IPs = set.intersection(*n_sets)
 
-file_data = get_reader(file_names[-1])
-for IP in [row[1] for row in file_data if ip_regex.match(row[1]) and row[14] == 'N']:
+for IP in n_dictionary[file_names[-1]]:
 	current_set.add(IP)
 
 ghost_ips = current_set - not_checked_in_IPs
@@ -36,7 +39,7 @@ for ip in ghost_ips:
 	missing = []
 	for file_name in file_names:
 		file_data = get_reader(file_name)
-		if ip not in [row[1] for row in file_data if ip_regex.match(row[1]) and row[14] == 'N']:
+		if ip not in n_dictionary[file_name]:
 			missing.append(file_name)
 	print (ip + " is missing from '" + missing[0] + "' and " + str(len(missing) - 1 )  + " others")
 
