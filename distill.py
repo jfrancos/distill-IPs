1880cf59802e9b10645c044a08b1b5c7d28a02d7
Justin Francos <jfrancos@mit.edu>
2018-08-16 12:19:17 -0400

    Added and adjusted to text output

--- distill.py
+++ distill.py
@@ -12,10 +12,14 @@ file_names = sorted((glob("../*-active_IPs.csv")))
 y_set = set()
 n_dictionary = {}
 hostname_dict = {}
+output_filename = 'active_IPs-{}.csv'.format(sys.argv[1])
 
 def get_reader(file_name):
 	# Remove null bytes for corruption in some files e.g. 2018-08-08
 	return csv.reader(x.replace('\0', '') for x in open(file_name))
+
+print ("\nProcessing {}".format(file_names[-1]))
+
 
 for file_name in file_names:
 	file_data = get_reader(file_name)
@@ -35,10 +39,11 @@ hosts_list = [(x, nm[x]['status']) for x in nm.all_hosts()]
 
 unpingable_IPs = not_checked_in_IPs - set(nm.all_hosts())
 
-print ("Removing {} down hosts: {}".format(len(unpingable_IPs), ', '.join(["{} ({})".format(ip, hostname_dict[ip]) for ip in unpingable_IPs])))
+
+print ("\nRemoving {} down hosts: {}".format(len(unpingable_IPs), ', '.join(["{} ({})".format(ip, hostname_dict[ip]) for ip in unpingable_IPs])))
 
 file_data = get_reader(file_names[-1])
-writer = csv.writer(open('current_distilled.csv', 'w'))
+writer = csv.writer(open(output_filename, 'w'))
 new_rows = []
 for row in file_data:
 	#if row[1] in not_checked_in_IPs and row[6] != 'CABLETRON':
@@ -49,6 +54,8 @@ writer.writerows((sorted_new_rows))
 
 
 
+print ("\nRemoving {} hosts for which there was a prior active_IPs list that didn't include it:".format(len(ghost_ips)))
+
 for ip in ghost_ips:
 	missing = []
 	for file_name in file_names:
@@ -56,3 +63,5 @@ for ip in ghost_ips:
 		if ip not in n_dictionary[file_name]:
 			missing.append(file_name)
 	print ("{} ({}) is missing from '{}' and {} others".format(ip, hostname_dict[ip], missing[0][3:], len(missing) - 1 ))
+
+print ("\nWrote {}".format(output_filename))
