ef454c651e850a232fae50f5ef11cde30da80c56
Justin Francos <jfrancos@mit.edu>
2018-08-19 11:36:58 -0400

    Has the triage sections: 1) has appeared on every active_IPs report and is up 2) is not in a list with a Y but either is absent from a list and/or is down 3) is explicitly on a list with a Y

--- distill.py
+++ distill.py
@@ -15,6 +15,7 @@ n_dictionary = {}
 hostname_dict = {}
 output_filename = 'active_IPs-{}.csv'.format(sys.argv[1])
 
+
 def get_reader(file_name):
 	# Remove null bytes for corruption in some files e.g. 2018-08-08
 	file = (x.replace('\0','') for x in open(file_name))
@@ -33,8 +34,11 @@ print('Processing {} past files.'.format(len(file_names) - 1), end='', flush=Tru
 # Creates a { filename -> set-of-ips dictionary, ... }, where all ips in the dictionaries match argv[1] and have 'N' for DHCP check-in
 for file_name in file_names:
 	print('.', end='', flush=True)
-	file_data = get_reader(file_name)
+	file_data = list(get_reader(file_name))
 	n_dictionary[file_name] = set(row['Address'] for row in file_data if ip_regex.match(row['Address']) and row['DHCP Check-in'] == 'N')
+	y_set |= set([row['Address'] for row in file_data if ip_regex.match(row['Address']) and row['DHCP Check-in'] == 'Y'])
+
+print (len(y_set))
 
 never_checked_in_IPs = set.intersection(*n_dictionary.values())
 current_set = n_dictionary[file_names[-1]]
@@ -58,17 +62,22 @@ print ("\nRemoving {} down hosts: {}".format(len(unpingable_IPs), ', '.join(["{}
 writer = csv.writer(open(output_filename, 'w'), quoting=csv.QUOTE_ALL)
 triage0_rows = []
 triage1_rows = []
-for row in [row for row in current_file_data if row['Address'] in current_IPs]:
+triage2_rows = []
+for row in [row for row in current_file_data if row['Address'] in current_IPs and row['Vendor'] != 'CABLETRON']:
 	#if row[1] in never_checked_in_IPs and row[6] != 'CABLETRON':
-	if row['Address'] not in unpingable_IPs and row['Address'] in never_checked_in_IPs and row['Vendor'] != 'CABLETRON':
+	if row['Address'] not in unpingable_IPs and row['Address'] in never_checked_in_IPs:
 		triage0_rows += [list(row.values())[1:-4] + ['Host up', 'Never checked in'] ]
-	elif row['Vendor'] != 'CABLETRON':
+	elif row['Address'] in y_set:
+		up = 'Host down' if row['Address'] in unpingable_IPs else 'Host up'
+		triage2_rows += [list(row.values())[1:-4] + [up, 'Has checked in'] ]
+	else:
 		up = 'Host down' if row['Address'] in unpingable_IPs else 'Host up'
 		checked = 'Never checked in' if row['Address'] in never_checked_in_IPs else 'May have checked in'
 		triage1_rows += [list(row.values())[1:-4] + [up, checked] ]
 sorted_triage0_rows = sorted(triage0_rows, key=lambda row: row[1])
 sorted_triage1_rows = sorted(triage1_rows, key=lambda row: row[1])
-writer.writerows(sorted_triage0_rows + sorted_triage1_rows)
+sorted_triage2_rows = sorted(triage2_rows, key=lambda row: row[1])
+writer.writerows(sorted_triage0_rows + sorted_triage1_rows + sorted_triage2_rows)
 
 print ("\nRemoving {} hosts for which there was a prior active_IPs list that didn't include it:".format(len(ghost_ips)))
 
